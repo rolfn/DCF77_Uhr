29c6b3fc88b89452da5095eece23abe61b643a98
Rolf Niepraschk <Rolf.Niepraschk@gmx.de>
2023-08-11 22:55:22 +0200

    tests ...

--- DCF77_Uhr.cpp
+++ DCF77_Uhr.cpp
@@ -1,116 +1,153 @@
+#include <Adafruit_i2c_7seg_LED.h>
+
 /**
  * DCF77_Uhr.cpp
  * Copyright:  Rolf Niepraschk <Rolf.Niepraschk AT gmx.de>
  * https://github.com/rolfn/DCF77_Uhr
  */
 
-#include <Arduino.h>
-#include "DCF77_Uhr.h"
 #include <Adafruit_i2c_7seg_LED.h>
+#include <Arduino.h>
 
-uint8_t rawADC = 0;
-uint8_t alarm_mode;
+#include "DCF77_Uhr.h"
+
+int rawADC = 0;
+int alarm_mode;
 float val = 0.0;
 
-Adafruit_7Seg DISP1;
-Adafruit_7Seg DISP2;
-Adafruit_7Seg DISP3;
+Adafruit_7Seg disp1;
+Adafruit_7Seg disp2;
+Adafruit_7Seg disp3;
 
-uint8_t getAlarmMode();
+int getAlarmMode();
 
-uint8_t getAlarmMode() {
-  uint8_t rawADC = analogRead(ALARM_MODE_PIN);
-  //float val = ((float) rawADC  + 0.5) / 1024.0 * AREF;
-  if (rawADC < LEVEL_20_PERCENT) return ALARM_MODE_DISABLED;
-  else if (rawADC > LEVEL_80_PERCENT) return ALARM_MODE_2;
-  else return ALARM_MODE_1;
+int getAlarmMode() {
+  int rawADC = analogRead(ALARM_MODE_PIN);
+  // float val = ((float) rawADC  + 0.5) / 1024.0 * AREF;
+  if (rawADC < LEVEL_20_PERCENT)
+    return ALARM_MODE_DISABLED;
+  else if (rawADC > LEVEL_80_PERCENT)
+    return ALARM_MODE_2;
+  else
+    return ALARM_MODE_1;
 }
 
-uint8_t xxx;
+int xxx;
 
-static const uint8_t ALARM1_SEL_PINS[] = { // ?
-  ALARM1_HOUR_HI, ALARM1_HOUR_LO, ALARM1_MINUTE_HI, ALARM1_MINUTE_LO
+static const int ALARM1_SEL_PINS[] = {  // ?
+  ALARM1_HOUR_HI_PIN, ALARM1_HOUR_LO_PIN, ALARM1_MINUTE_HI_PIN,
+  ALARM1_MINUTE_LO_PIN
 };
 
-static const uint8_t ALARM2_SEL_PINS[] = { // ?
-  ALARM2_HOUR_HI, ALARM2_HOUR_LO, ALARM2_MINUTE_HI, ALARM2_MINUTE_LO
+static const int ALARM2_SEL_PINS[] = {  // ?
+  ALARM2_HOUR_HI_PIN, ALARM2_HOUR_LO_PIN, ALARM2_MINUTE_HI_PIN,
+  ALARM2_MINUTE_LO_PIN
 };
 
 void setup() {
   analogReference(DEFAULT);
   Serial.begin(9600);
-  while (!Serial); 
+  while (!Serial);
   Serial.println(__FILE__);
-  DISP1.begin(0x00); DISP2.begin(0x01); DISP3.begin(0x02);
-  DISP1.setBrightness(16); // 0..16
-  DISP2.setBrightness(3); // 0..16
-  DISP3.setBrightness(3); // 0..16
-  DISP1.setPoint(COLON); DISP2.setPoint(COLON); DISP3.setPoint(COLON);
+  disp1.begin(DISP1_ADR);
+  disp2.begin(DISP2_ADR);
+  disp3.begin(DISP3_ADR);
+  disp1.setBrightness(16);  // 0..16
+  disp2.setBrightness(3);   // 0..16
+  disp3.setBrightness(3);   // 0..16
+  disp1.setPoint(COLON);
+  disp2.setPoint(COLON);
+  disp3.setPoint(COLON);
   //
   pinMode(BCD1, INPUT_PULLUP);
   pinMode(BCD2, INPUT_PULLUP);
   pinMode(BCD4, INPUT_PULLUP);
-  pinMode(BCD8, INPUT_PULLUP); 
-  pinMode(ALARM_OFF_PIN, INPUT_PULLUP); 
-  DDRD = DDRD | B11111100; // PD2..PD7 as output
-  DDRB = DDRB | B00000011; // PB0..PB1 as output
-  pinMode(DCF77_MONITOR_LED, OUTPUT);
-  pinMode(DCF77_SAMPLE_PIN
+  pinMode(BCD8, INPUT_PULLUP);
+  pinMode(ALARM_OFF_PIN, INPUT_PULLUP);
+  DDRD = DDRD | B11111100;             // PD2..PD7 as output (BCD selection)
+  DDRB = DDRB | B00000011;             // PB0..PB1 as output (BCD selection)
+  pinMode(DCF77_MONITOR_LED, OUTPUT);  // nötig?
+  //pinMode(DCF77_SAMPLE_PIN, INPUT_PULLUP);  // nötig?
 }
 
 void loop() {
   // turn the LED on (HIGH is the voltage level)
   digitalWrite(DCF77_MONITOR_LED, HIGH);
-  
-  DISP1.setPoint(POINT_UPPER_LEFT);
-  DISP1.clearPoint(POINT_LOWER_LEFT);
 
-  xxx = 3;
+  disp1.setPoint(POINT_UPPER_LEFT);
+  disp1.clearPoint(POINT_LOWER_LEFT);
 
-  DISP1.setDigit(DIGIT_1, ++xxx); delay(500);
-  DISP1.setDigit(DIGIT_2, ++xxx); delay(500);
-  DISP1.setDigit(DIGIT_3, ++xxx); delay(500);
-  DISP1.setDigit(DIGIT_4, ++xxx); delay(500);
+  xxx = 0;
 
-  DISP2.setDigit(DIGIT_1, ++xxx, true); delay(500);
-  DISP2.setDigit(DIGIT_2, ++xxx, true); delay(500);
-  DISP2.setDigit(DIGIT_3, ++xxx, true); delay(500);
-  DISP2.setDigit(DIGIT_4, ++xxx, true); delay(500);
+  disp1.setDigit(DIGIT_1, ++xxx);
+  delay(300);
+  disp1.setDigit(DIGIT_2, ++xxx);
+  delay(300);
+  disp1.setDigit(DIGIT_3, ++xxx);
+  delay(300);
+  disp1.setDigit(DIGIT_4, ++xxx);
+  delay(300);
 
-  DISP3.setDigit(DIGIT_1, ++xxx, true); delay(500);
-  DISP3.setDigit(DIGIT_2, ++xxx, true); delay(500);
-  DISP3.setDigit(DIGIT_3, ++xxx, true); delay(500);
-  DISP3.setDigit(DIGIT_4, ++xxx, true); delay(500);
+  disp2.setDigit(DIGIT_1, ++xxx, true);
+  delay(300);
+  disp2.setDigit(DIGIT_2, ++xxx, true);
+  delay(300);
+  disp2.setDigit(DIGIT_3, ++xxx, true);
+  delay(300);
+  disp2.setDigit(DIGIT_4, ++xxx, true);
+  delay(300);
+
+  disp3.setDigit(DIGIT_1, ++xxx, true);
+  delay(300);
+  disp3.setDigit(DIGIT_2, ++xxx, true);
+  delay(300);
+  disp3.setDigit(DIGIT_3, ++xxx, true);
+  delay(300);
+  disp3.setDigit(DIGIT_4, ++xxx, true);
+  delay(300);
+
+  delay(3500);
 
   // turn the LED off by making the voltage LOW
   digitalWrite(DCF77_MONITOR_LED, LOW);
 
-  DISP1.clearPoint(POINT_UPPER_LEFT);
-  DISP1.setPoint(POINT_LOWER_LEFT);
+  disp1.clearPoint(POINT_UPPER_LEFT);
+  disp1.setPoint(POINT_LOWER_LEFT);
 
-  DISP1.clearPosition(DIGIT_1); delay(500);
-  DISP1.clearPosition(DIGIT_2); delay(500);
-  DISP1.clearPosition(DIGIT_3); delay(500);
-  DISP1.clearPosition(DIGIT_4); delay(500);
-  
-  DISP2.clearPosition(DIGIT_1); delay(500);
-  DISP2.clearPosition(DIGIT_2); delay(500);
-  DISP2.clearPosition(DIGIT_3); delay(500);
-  DISP2.clearPosition(DIGIT_4); delay(500);
+  disp1.clearPosition(DIGIT_1);
+  delay(300);
+  disp1.clearPosition(DIGIT_2);
+  delay(300);
+  disp1.clearPosition(DIGIT_3);
+  delay(300);
+  disp1.clearPosition(DIGIT_4);
+  delay(300);
 
-  DISP3.clearPosition(DIGIT_1); delay(500);
-  DISP3.clearPosition(DIGIT_2); delay(500);
-  DISP3.clearPosition(DIGIT_3); delay(500);
-  DISP3.clearPosition(DIGIT_4); delay(500);
+  disp2.clearPosition(DIGIT_1);
+  delay(300);
+  disp2.clearPosition(DIGIT_2);
+  delay(300);
+  disp2.clearPosition(DIGIT_3);
+  delay(300);
+  disp2.clearPosition(DIGIT_4);
+  delay(300);
 
-  //DISP1.clearAll();
+  disp3.clearPosition(DIGIT_1);
+  delay(300);
+  disp3.clearPosition(DIGIT_2);
+  delay(300);
+  disp3.clearPosition(DIGIT_3);
+  delay(300);
+  disp3.clearPosition(DIGIT_4);
+  delay(300);
 
-   // wait for a second
-  delay(1000);
+  // disp1.clearAll();
 
-  rawADC = analogRead(ALARM_MODE_PIN);
-  val = ((float) rawADC  + 0.5 ) / 1024.0 * AREF;
+  // wait for a second
+  delay(500);
+
   alarm_mode = getAlarmMode();
+
   Serial.print(millis());
   Serial.print("   ");
   Serial.print(alarm_mode);
--- DCF77_Uhr.h
+++ DCF77_Uhr.h
@@ -7,35 +7,39 @@
 #ifndef DCF77_UHR_H
 #define DCF77_UHR_H
 
-#define DCF77_MONITOR_LED PB4
+#define DCF77_MONITOR_LED 12 // PB4
 //#define DCF77_MONITOR_LED LED_BUILTIN
-#define DCF77_SAMPLE_PIN PB5 // ???
+#define DCF77_SAMPLE_PIN 13  // PB5 (???)
 //#define DCF77_INVERTED_SAMPLES 1
 
-#define BCD1 PC0
-#define BCD2 PC1
-#define BCD4 PC2
-#define BCD8 PC3
+#define BCD1 14 // PC0
+#define BCD2 15 // PC1
+#define BCD4 16 // PC2
+#define BCD8 17 // PC3
 
-#define ALARM1_HOUR_HI   PD2
-#define ALARM1_HOUR_LO   PD3
-#define ALARM1_MINUTE_HI PD4
-#define ALARM1_MINUTE_LO PD5
+#define ALARM1_HOUR_HI_PIN   2 // PD2
+#define ALARM1_HOUR_LO_PIN   3 // PD3
+#define ALARM1_MINUTE_HI_PIN 4 // PD4
+#define ALARM1_MINUTE_LO_PIN 5 // PD5
 
-#define ALARM2_HOUR_HI   PD6
-#define ALARM2_HOUR_LO   PD7
-#define ALARM2_MINUTE_HI PB0
-#define ALARM2_MINUTE_LO PB1
+#define ALARM2_HOUR_HI_PIN   6 // PD6
+#define ALARM2_HOUR_LO_PIN   7 // PD7
+#define ALARM2_MINUTE_HI_PIN 8 // PB0
+#define ALARM2_MINUTE_LO_PIN 9 // PB1
 
 #define ALARM_MODE_PIN A6
 #define ALARM_MODE_DISABLED 0
 #define ALARM_MODE_1 1
 #define ALARM_MODE_2 2
-#define ALARM_OFF_PIN PB2
+#define ALARM_OFF_PIN 10 // PB2
 
 #define LEVEL_20_PERCENT 205 // 1023 * 20 / 100 
 #define LEVEL_80_PERCENT 818 // 1023 * 80 / 100
 #define AREF 5.0
+
+#define DISP1_ADR 0x00 // --> 0x70
+#define DISP2_ADR 0x01 // --> 0x71
+#define DISP3_ADR 0x02 // --> 0x72
 
 #endif
 
